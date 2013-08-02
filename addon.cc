8921665ce8a1665d155949606c1c45a69c9d99bf
m0ose <m0ose@yahoo.com>
2013-08-02 16:54:09 -0600

    total rewrite

--- addon.cc
+++ addon.cc
@@ -1,11 +1,19 @@
 #define BUILDING_NODE_EXTENSION
 #include <node.h>
-#include <stdio.h>
-#include "fireflywrap.h"
+#include "FF2wrapper.h"
+
 using namespace v8;
 
-void InitAll(Handle<Object> exports) {
-    FireFlyWrap::Init(exports);
+Handle<Value> CreateObject(const Arguments& args) {
+  HandleScope scope;
+  return scope.Close(FF2wrapper::NewInstance(args));
+}
+
+void InitAll(Handle<Object> exports, Handle<Object> module) {
+  FF2wrapper::Init();
+
+  module->Set(String::NewSymbol("exports"),
+      FunctionTemplate::New(CreateObject)->GetFunction());
 }
 
 NODE_MODULE(addon, InitAll)
